232d4a365adf257b016f980d142dd3f8637ced5a
Mohamad Bashar Desoki <mhdbashard@gmail.com>
2022-11-03 14:40:14 +0200

    add project files

---
+++ src/main/java/com/example/demo/controller/homeController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author skybar
+ */
+@RestController
+@RequestMapping("/")
+public class homeController {
+    
+    @GetMapping
+    public String home() {
+        return "<h1>Hello DS-2022 </h1>";
+    }
+}
